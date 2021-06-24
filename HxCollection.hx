9ecf274ec64a83db6f027a212ccb6f25909c4d6c
phamvanthanh <pthanh.ce@gmail.com>
2021-06-24 12:14:24 +0700

    remove test trace

--- HxCollection.hx
+++ HxCollection.hx
@@ -31,7 +31,7 @@ class HxCollection {
 
     @:generic
     public static function toHaxeArray<T>(narr:NativeArray<T>):Array<T> {
-        var arr:Array<T> = [];
+        var arr:Array<T> = [narr.length];
         
         for(i in 0...narr.length){
             arr[i] = narr[i];
